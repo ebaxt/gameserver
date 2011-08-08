911179c20b0093313efe35ed970deee008a1025a
Erik Bakstad <erik.bakstad@gmail.com>
2011-08-08 21:31:44 +0200

    Messing arround

--- src/gameserver/gameserver.clj
+++ src/gameserver/gameserver.clj
@@ -1,37 +1,77 @@
 (ns gameserver.gameserver
   (:use [clojure.java.io :only [reader writer]]
-    [clojure.contrib.server-socket :only [create-server]]))
+    [clojure.contrib.server-socket :only [create-server connection-count close-server]]))
 
-(defn add [a b] (* a b))
-;(defn dvd [a b] (/ a b))
-;
-;
-;(defn- mire-handle-client [in out]
-;  (binding [*in* (reader in)
-;            *out* (writer out)]
-;
-;    ;; We have to nest this in another binding call instead of using
-;    ;; the one above so *in* and *out* will be bound to the socket
-;    (print "\nWhat is your name? ") (flush)
-;    (binding [*player-name* (get-unique-player-name (read-line))]
-;      (dosync
-;       (commute (:inhabitants @*current-room*) conj *player-name*)
-;       (commute player-streams assoc *player-name* *out*))
-;
-;      (println (look)) (print prompt) (flush)
-;
-;      (try (loop [input (read-line)]
-;             (when input
-;               (println (execute input))
-;               (print prompt) (flush)
-;               (recur (read-line))))
-;           (finally (cleanup))))))
-;
-;
-;
-;(defn -main
-;  ([port]
-;     (add-rooms dir)
-;     (defonce server (create-server (Integer. port) mire-handle-client))
-;     (println "Launching game server on port" port))
-;  ([] (-main 3333)))
+(def players (ref {}))
+(def last-call (ref {}))
+(def score (ref {}))
+(def tools #(:stone :paper :sissors))
+
+(defn- get-unique-player-name [name]
+  (if (@players name)
+    (do (print "That name is in use; try again: ")
+      (flush)
+      (recur (read-line)))
+    name))
+
+
+(defn send-to-players [message players]
+  (doseq [player players]
+    (binding [*out* (get-in player [1 :out])]
+      (println (str message player)) (flush))))
+
+(defn execute [command player]
+  (str player ":" command))
+
+(defn validate-input [valid-inputs input]
+  (if (contains? valid-inputs input)
+    input
+    (do (println ":invalid-input")
+      (flush)
+      (recur valid-inputs (read-line)))))
+
+(defn select [player]
+  (println :select) (flush)
+  (let [tool (validate-input tools (read-line))]
+    (dosync
+      last-call assoc player tool)
+    (simulate-game player :evaluate)))
+
+(defn wait [player pred next]
+  (if (pred)
+    (next)
+    (recur player pred next)))
+
+(defn evaluate [player]
+  (if )
+  )
+
+(defn simulate-game [player state & r]
+  (case state
+    :select (select player)
+    :wait (wait player #(= (count @last-call) 2) #(simulate-game player :evaluate))
+    :evaluate (evaluate player)
+    ))
+
+
+(defn wait-for-enough-players []
+  (while (< (count @players) 2)
+    (println ":waiting")
+    (flush)
+    (Thread/sleep 2000)))
+
+(def register-players (fn [in out]
+  (binding [*in* (reader in)
+            *out* (writer out)]
+    (print "\nEnter player name: ") (flush)
+    (let [player (get-unique-player-name (read-line))]
+      (dosync
+        (alter players assoc player {:in *in* :out *out*}))
+      (wait-for-enough-players) (simulate-game player :start)))))
+
+
+(defn -main
+  ([port]
+    (def server (create-server (Integer. port) register-players))
+    (println "Launching game server on port" port))
+  ([] (-main 3333)))
