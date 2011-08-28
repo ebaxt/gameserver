2a08acae596b87ac0d7f8f7fbbd046f838f3235f
Erik Bakstad <erik.bakstad@gmail.com>
2011-08-28 22:54:36 +0200

    Refactored

--- src/gameserver/gameserver.clj
+++ src/gameserver/gameserver.clj
@@ -3,21 +3,21 @@
     [clojure.contrib.server-socket :only (create-server connection-count close-server)]
     [clojure.tools.logging :only (info)]))
 
-(def players (atom {}))
-
 (def *oponent*)
 (def *player*)
+
+(def players (atom {}))
 (def tools #{:rock :paper :scissors})
 (def rules {:rock :scissors :scissors :paper :paper :rock})
 
 (defn- get-unique-player-name [name]
   (if (@players name)
-    (do (println "That name is already in use, try again: ")
-      (flush)
+    (do
+      (println "That name is already in use, try again: ") (flush)
       (recur (read-line)))
     name))
 
-(defn oponent [player]
+(defn find-oponent [player]
   (first (filter #(not= % player) (keys @players))))
 
 (defn find-result [player-move oponent-move]
@@ -27,21 +27,21 @@
       :winner
       :looser)))
 
+(defn wait-until-oponent [action]
+  (while (not (get-in @players [*oponent* action]))))
+
 (declare make-selection)
 
 (defn clear-and-restart []
-  (while (not (get-in @players [*oponent* :reported])))
-  (swap! players assoc-in [*oponent* :reported] false)
-  (swap! players assoc-in [*oponent* :selection] nil)
-  (swap! players assoc-in [*oponent* :result] nil))
+  (wait-until-oponent :reported)
+  (swap! players update-in [*oponent*] assoc :reported false :selected nil :result nil))
 
 (defn evaluate []
-  (while (not (get-in @players [*oponent* :selection])))
-  (let [player-move (get-in @players [*player* :selection])
-        oponent-move (get-in @players [*oponent* :selection])
+  (wait-until-oponent :selected)
+  (let [player-move (get-in @players [*player* :selected])
+        oponent-move (get-in @players [*oponent* :selected])
         result (find-result player-move oponent-move)]
-    (swap! players assoc-in [*player* :result] result)
-    (swap! players assoc-in [*player* :reported] true)
+    (swap! players update-in [*player*] assoc :result result :reported true)
     (println [result oponent-move]) (flush)
     (if (= result :winner)
       (swap! players update-in [*player* :score] inc)))
@@ -55,30 +55,34 @@
       (recur valid-inputs (read-line)))))
 
 (defn make-selection [rounds-left]
-  (while (get-in @players [*player* :selection]))
+  (while (get-in @players [*player* :selected]))
   (println ":select") (flush)
   (let [tool (validate-input tools (read-line))]
-    (swap! players assoc-in [*player* :selection] tool))
+    (swap! players assoc-in [*player* :selected] tool))
   (evaluate)
   (dec rounds-left))
 
-(defn wait-for-other-player [player]
+(defn wait-for-other-player []
   (while (< (count @players) 2)
     (println ":waiting")
     (flush)
-    (Thread/sleep 2000))
-  (binding [*oponent* (oponent player)
+    (Thread/sleep 2000)))
+
+(defn play-rounds [rounds player]
+  (binding [*oponent* (find-oponent player)
             *player* player]
-    (loop [rounds-left 100000]
+    (loop [rounds-left rounds]
       (when (pos? rounds-left)
         (recur (make-selection rounds-left))))))
 
-(def register-players (fn [in out]
+(defn register-players [in out]
   (binding [*in* (reader in)
             *out* (writer out)]
     (println "Enter player name: ") (flush)
     (let [player (get-unique-player-name (read-line))]
-      (swap! players assoc player {:selection nil :result nil :score 0 :reported false})
-      (wait-for-other-player player)))))
+      (swap! players assoc player {:selected nil :result nil :score 0 :reported false})
+      (wait-for-other-player)
+      (play-rounds 100 player)))
+  (println @players))
 
 (def server (create-server 3333 register-players))
